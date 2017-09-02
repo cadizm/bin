964facb31b3c1746ec61b7c671d25a76531d44e1
Michael Cadiz <mike@cadizm.com>
2017-09-02 11:48:40 -0700

    sync 2017-09-02

--- fixtags.py
+++ fixtags.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python
 
 import sys
 import tempfile
 
 if __name__ == '__main__':
     t = tempfile.TemporaryFile()
+    print("fixing %s" % sys.argv[1])
     with open(sys.argv[1], 'r') as fh:
         for line in fh:
             if line.find('\t') != -1:
