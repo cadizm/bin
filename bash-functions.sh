6b359e2e04493925716f4f2f1dbf65d0d1bb2ca1
Michael Cadiz <mike@cadizm.com>
2020-05-29 12:36:09 -0700

    don't change directories after git clone/init

--- bash-functions.sh
+++ bash-functions.sh
@@ -14,12 +14,13 @@ function git() {
       if [[ -d "$lastArgument" ]]
       then
         # it was the directory argument, cd it
-        cd $lastArgument
+        pushd $lastArgument
       else
         # no directory given, parse it from repository url
-        cd $(echo $lastArgument | awk -F/ '{ print $NF }' | rev | sed 's/tig.//' | rev)
+        pushd $(echo $lastArgument | awk -F/ '{ print $NF }' | rev | sed 's/tig.//' | rev)
       fi
       git-email-prompt.sh
+      popd
     fi
   else
     # return the exit code of the failed git command call
